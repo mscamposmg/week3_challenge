08461fbe927b7c85942820129f34cc3def6bade9
mscamposmg <133654984+mscamposmg@users.noreply.github.com>
2023-06-30 00:36:38 +1000

    eighth commit script.js
    add a function engenderPass

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function engenderPass() {
+  
 // Creating a list of Variables using ARRAY
 const capitalLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const smallLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -36,7 +38,7 @@ for (var i = 0; i < numCharacter; i++) {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = engenderPass();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
