44ff9d4ba84b8639a3ce9471e8fb15d6d46fdbef
mscamposmg <133654984+mscamposmg@users.noreply.github.com>
2023-06-29 23:44:21 +1000

    third commit script.js
    Initializing the variable array's type

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 
 // Creating a list of Variables using ARRAY
-
 const capitalLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const smallLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const numeral = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~", "`", "|", "}", "{", "[", "]", ":", ";", "?", ">", "<", ",", ".", "/", "-", "="];
 
+// Initializing the variables of type array
+var effectArray = [];
+var costumerArray = [];
 
 // Write password to the #password input
 function writePassword() {
